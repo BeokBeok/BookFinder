16b7c76ccbf7dbe63bf834e8dc318a8cad3fe096
BeokBeok <kekemusa37@nate.com>
2020-08-17 00:42:26 +0900

    [적용] 검색 결과 개수

--- app/src/main/java/com/beok/bookfinder/BookSearchViewModel.kt
+++ app/src/main/java/com/beok/bookfinder/BookSearchViewModel.kt
@@ -5,12 +5,20 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.beok.bookfinder.model.BookItem
+import com.beok.bookfinder.model.mapToPresenter
 import com.beok.common.safeLaunch
 import com.beok.domain.BooksRepository
 
 class BookSearchViewModel @ViewModelInject constructor(
     private val booksRepository: BooksRepository
 ) : ViewModel() {
+
+    private val _book = MutableLiveData<List<BookItem>>()
+    val book: LiveData<List<BookItem>> get() = _book
+
+    private val _resultCount = MutableLiveData(0)
+    val resultCount: LiveData<Int> get() = _resultCount
 
     private val _errMessage = MutableLiveData<String>()
     val errMessage: LiveData<String> get() = _errMessage
@@ -25,5 +33,10 @@ class BookSearchViewModel @ViewModelInject constructor(
             _errMessage.value = result.exceptionOrNull()?.message ?: ""
             return@safeLaunch
         }
+        result.map { it.mapToPresenter() }.getOrNull()?.let {
+            _resultCount.value = it.totalItems
+            _book.value = it.items
+        }
+
     }
 }
