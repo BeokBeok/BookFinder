5c896c76397528b73bc5b8be6ad49346b9a63969
BeokBeok <kekemusa37@nate.com>
2020-08-17 02:01:04 +0900

    [수정] 작가 리스트 -> 하나의 문자열

--- app/src/main/java/com/beok/bookfinder/model/BookItem.kt
+++ app/src/main/java/com/beok/bookfinder/model/BookItem.kt
@@ -4,14 +4,14 @@ import com.beok.domain.entity.ItemsItem
 
 data class BookItem(
     val title: String,
-    val authors: List<String>,
+    val authors: String,
     val publishedDate: String,
     val imageUrl: String
 )
 
 fun ItemsItem.mapToPresenter() = BookItem(
     title = volumeInfo.title,
-    authors = volumeInfo.authors,
+    authors = volumeInfo.authors.joinToString(),
     publishedDate = volumeInfo.publishedDate,
     imageUrl = volumeInfo.imageLinks.thumbnail
 )
