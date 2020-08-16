20bb3127c27c4c20430c830fba0c0dd397397fbd
BeokBeok <kekemusa37@nate.com>
2020-08-16 15:30:15 +0900

    [추가] GoogleBooksInterceptor

---
+++ domain/src/main/java/com/beok/domain/interceptor/GoogleBooksInterceptor.kt
@@ -0,0 +1,24 @@
+package com.beok.domain.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GoogleBooksInterceptor @Inject constructor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val url = chain.request().url()
+            .newBuilder()
+            .addQueryParameter("key", API_KEY)
+            .build()
+
+        val request = chain.request().newBuilder().url(url).build()
+        return chain.proceed(request)
+    }
+
+    companion object {
+        private const val API_KEY = "AIzaSyBTssaXMuVQ9LAesQPUUAsTvXMwP99AJDM"
+    }
+}
