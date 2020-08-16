21be6f631a0b887dfc8b48c458c2d56c1c384f03
BeokBeok <kekemusa37@nate.com>
2020-08-17 00:29:57 +0900

    [적용] BookSearchActivity 에 데이터바인딩

--- app/src/main/java/com/beok/bookfinder/BookSearchActivity.kt
+++ app/src/main/java/com/beok/bookfinder/BookSearchActivity.kt
@@ -1,11 +1,27 @@
 package com.beok.bookfinder
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.beok.bookfinder.databinding.ActivityBookSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class BookSearchActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<BookSearchViewModel>()
+    private lateinit var binding: ActivityBookSearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book_search)
+
+        setupBinding()
+    }
+
+    private fun setupBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_book_search)
+        binding.lifecycleOwner = this
+        binding.vm = viewModel
     }
 }
