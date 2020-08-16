02620a2652c6da3bea3064dd136bec69324c738c
BeokBeok <kekemusa37@nate.com>
2020-08-16 17:02:20 +0900

    [변경] Main to BookSearch

--- app/src/main/java/com/beok/bookfinder/BookSearchActivity.kt
+++ app/src/main/java/com/beok/bookfinder/BookSearchActivity.kt
@@ -3,9 +3,9 @@ package com.beok.bookfinder
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class BookSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_book_search)
     }
 }
