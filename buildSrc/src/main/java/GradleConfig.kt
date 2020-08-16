56e40e8ae94d6b8c9f6189df4a466f1c9abd5779
BeokBeok <kekemusa37@nate.com>
2020-08-17 00:55:41 +0900

    [추가] Glide 디펜던시

--- buildSrc/src/main/java/GradleConfig.kt
+++ buildSrc/src/main/java/GradleConfig.kt
@@ -20,6 +20,8 @@ object AndroidConfig {
 }
 
 object Dependencies {
+    private const val GLIDE_VER = "4.11.0"
+
     const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:${Version.KOTLIN}"
 
     const val MATERIAL = "com.google.android.material:material:1.1.0"
@@ -29,6 +31,9 @@ object Dependencies {
     const val COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9"
 
     const val TIMBER = "com.jakewharton.timber:timber:4.7.1"
+
+    const val GLIDE = "com.github.bumptech.glide:glide:$GLIDE_VER"
+    const val GLIDE_COMPILER = "com.github.bumptech.glide:compiler:$GLIDE_VER"
 }
 
 object TestDependencies {
