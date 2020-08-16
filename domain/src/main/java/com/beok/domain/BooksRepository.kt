5db2f570f00600ccc92e932bb2807bbbfb294529
BeokBeok <kekemusa37@nate.com>
2020-08-16 16:10:52 +0900

    [추가] BooksRepository

---
+++ domain/src/main/java/com/beok/domain/BooksRepository.kt
@@ -0,0 +1,17 @@
+package com.beok.domain
+
+import com.beok.domain.entity.BooksResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class BooksRepository @Inject constructor(private val booksService: BooksService) :
+    BooksDataSource {
+
+    private val ioDispatcher = Dispatchers.IO
+
+    override suspend fun searchBook(bookName: String): Result<BooksResponse> =
+        withContext(ioDispatcher) {
+            runCatching { booksService.search(bookName) }
+        }
+}
