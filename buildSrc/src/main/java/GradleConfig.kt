56277eb5add2f48a058ee09b67db65efb1cbb8d5
BeokBeok <kekemusa37@nate.com>
2020-08-16 23:26:27 +0900

    [추가] activity-ktx 디펜던시

--- buildSrc/src/main/java/GradleConfig.kt
+++ buildSrc/src/main/java/GradleConfig.kt
@@ -49,6 +49,8 @@ object JetpackDependencies {
 
     const val LIFECYCLE_EXT = "androidx.lifecycle:lifecycle-extensions:$LIFECYCLE_VER"
     const val LIFECYCLE_VIEWMODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:$LIFECYCLE_VER"
+
+    const val ACTIVITY_KTX = "androidx.activity:activity-ktx:1.2.0-alpha07"
 }
 
 object NetworkDependencies {
