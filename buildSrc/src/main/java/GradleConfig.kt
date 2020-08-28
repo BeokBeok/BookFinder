3e3da11fddbefea5b1bbe43ed5b745e716439959
BeokBeok <kekemusa37@nate.com>
2020-08-28 13:57:36 +0900

    [추가] Junit5, MockWebServer

--- buildSrc/src/main/java/GradleConfig.kt
+++ buildSrc/src/main/java/GradleConfig.kt
@@ -39,9 +39,9 @@ object Dependencies {
 }
 
 object TestDependencies {
-    const val JUNIT = "junit:junit:4.13"
-    const val EXT_JUNIT = "androidx.test.ext:junit:1.1.1"
-    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.2.0"
+    const val JUNIT_JUPITER = "org.junit.jupiter:junit-jupiter:5.6.2"
+    const val ASSERTJ_CORE = "org.assertj:assertj-core:3.16.1"
+    const val MOCK_WEB_SERVER = "com.squareup.okhttp3:mockwebserver:4.8.1"
 }
 
 object JetpackDependencies {
