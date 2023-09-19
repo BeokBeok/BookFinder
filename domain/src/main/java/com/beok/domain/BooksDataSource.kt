75aaa748afc830107a31e750d628b46ec3f3cd2a
강현석 <kekemusa37@gmail.com>
2023-09-19 22:37:16 +0900

    2

--- domain/src/main/java/com/beok/domain/BooksDataSource.kt
+++ domain/src/main/java/com/beok/domain/BooksDataSource.kt
@@ -3,6 +3,5 @@ package com.beok.domain
 import com.beok.domain.entity.BooksResponse
 
 interface BooksDataSource {
-
     suspend fun searchBook(keyword: String, page: Int = 0, perPage: Int = 10): Result<BooksResponse>
 }
